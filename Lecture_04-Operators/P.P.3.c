e4584027f9bc125eac2cb239daebc51bebba39a3
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-01-30 07:42:52 +0600

    Add files via upload

---
+++ Lecture_04-Operators/P.P.3.c
@@ -0,0 +1,11 @@
+//What is the output of the following program?
+#include <stdio.h>
+int main()
+{
+    int a = 2, b = 2, c = 2;
+    printf("%d\n", a == b == c);
+    return 0;
+}
+/*Output is:
+0
+*/
