1fff9b46b6de91e9754a20d36e94d6353b16c454
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-02 09:44:22 +0600

    Delete Lecture_06-Loops/1.c

--- Lecture_06-Loops/1.c
+++
@@ -1,9 +0,0 @@
-//Write a program that displays the integers from 10 down to 1.
-#include <stdio.h>
-int main()
-{
-    int i;
-   for(i = 10; i>= 1; i--)
-        printf("%d\n", i);
-    return 0;
-}
