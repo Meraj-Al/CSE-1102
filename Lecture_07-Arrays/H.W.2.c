752299bb826669ab1cee824143b51b249ace79c1
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-03 06:52:44 +0600

    Delete Lecture_07-Arrays/H.W.2.c

--- Lecture_07-Arrays/H.W.2.c
+++
@@ -1,9 +0,0 @@
-//What would be the values of the array arr in the following program?
-#include <stdio.h>
-int main()
-{
-    int i, arr[3] = {0, 1, 2};
-    for (i = 1; i < 4; i++)
-        arr[arr[3 - i]] = i - 1;
-}
-//Output of arr is : {2,1,0}
