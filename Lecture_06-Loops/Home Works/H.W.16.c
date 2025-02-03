93858471c75b7909b5f2385855a5f02fe1c43e63
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-03 06:59:58 +0600

    Add files via upload

---
+++ Lecture_06-Loops/Home Works/H.W.16.c
@@ -0,0 +1,27 @@
+/*
+Write a program that reads an integer (i.e., N) and displays the result of:
+1/1*3+1/3*5+1/5*7+ ... +1/(N − 2) ∗ N
+*/
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    double sum = 0.0;
+
+
+    do {
+        printf("Enter an odd integer (>=3): ");
+        scanf("%d", &N);
+    } while (N < 3 || N % 2 == 0);
+
+    for (int i = 1; i <= N - 2; i += 2) {
+        sum += 1.0 / (i * (i + 2));
+    }
+
+
+    printf("The result of the series is: %.6f\n", sum);
+
+    return 0;
+}
+
