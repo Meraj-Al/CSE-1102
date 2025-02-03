ae0d286cbad6910bb67f414514137ee4b2332864
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-03 06:59:36 +0600

    Add files via upload

---
+++ Lecture_06-Loops/Home Works/H.W.15.c
@@ -0,0 +1,43 @@
+/*
+Write a program that reads two integers (i.e., M, N) and produces an M×N grid. Each
+grid cell should be 3×2 characters wide. As an example, a 3×5 grid follows:
++−−+−−+−−+−−+−−+
+| | | | | |
++−−+−−+−−+−−+−−+
+| | | | | |
++−−+−−+−−+−−+−−+
+| | | | | |
++−−+−−+−−+−−+−−+
+The three horizontal characters of each cell should be +−− and the two verticals +|
+*/
+#include <stdio.h>
+
+int main()
+{
+    int R, C, i, j;
+
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d%d", &R,&C);
+
+    for (int i = 0; i <= R; i++)
+    {
+        for (int j = 0; j <=C; j++)
+        {
+            printf("+--");
+        }
+        printf("+\n");
+
+        if (i < R)
+        {
+            for (int j = 0; j < C; j++)
+            {
+                printf("|  ");
+            }
+            printf("|\n");
+        }
+    }
+
+    return 0;
+}
+
