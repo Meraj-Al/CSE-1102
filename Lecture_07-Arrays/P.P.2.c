d9526fd333563eb1884a3b0150fed48c98634ea2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-03 06:53:29 +0600

    Delete Lecture_07-Arrays/P.P.2.c

--- Lecture_07-Arrays/P.P.2.c
+++
@@ -1,9 +0,0 @@
-//What would be the values of the array arr in the following program?
-#include <stdio.h>
-int main()
-{
-    int i, arr[10] = {0};
-    for(i = 0; i < 10; i++)
-        arr[i++] = 20;
-    return 0;
-}
