2628b15d235917d25fdaf4c4e48ce2b7328228ad
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-04 09:58:24 +0600

    Update 3.c

--- Lecture_02: Data Types, Variables and Data Output/Homeworks/3.c
+++ Lecture_02: Data Types, Variables and Data Output/Homeworks/3.c
@@ -8,7 +8,9 @@ int main()
     printf("%d %d\n", k, (int)(i + (int)j));
     return 0;
 }
-/* int i convert the i from float to int. So, k = 10+20.3=30(not 30.3, because k is declared as integer.)Because the value of the cast expression (int)(i+(int)j) is
-(int)(10.9+(int)20.3) = (int)(10.9+20) = (int)(30.9) = 30.
+//Output is 30 30.
 
-so the answer is 30 30*/
+/* int i convert the i from float to int. So, k = 10+20.3=30(not 30.3, because k is declared as integer.)Because the value of the cast expression (int)(i+(int)j) is
+(int)(10.9+(int)20.3) = (int)(10.9+20) = (int)(30.9) = 30.*/
+
+
