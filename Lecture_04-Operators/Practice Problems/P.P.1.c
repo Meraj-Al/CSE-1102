37ba71ca3b5d69bfbe306353234434a5387acb25
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-13 08:26:38 +0600

    Update P.P.1.c

--- Lecture_04-Operators/Practice Problems/P.P.1.c
+++ Lecture_04-Operators/Practice Problems/P.P.1.c
@@ -1,9 +1,11 @@
 //What is the output of the following program?
+
 #include <stdio.h>
 int main()
 {
     int a = 3, b = 5, c;
     a = (a > 3) + (b <= 5);
+    
     b = (a== 3) + ((b-2) >= 3);
     c = (b != 1);
     printf("%d %d %d\n", a, b, c);
