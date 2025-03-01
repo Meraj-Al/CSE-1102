d4f4379fcc2a690e63e76103c199e578c3a634ff
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-01 12:24:58 +0600

    Add files via upload

---
+++ Lecture_10-Functions(M)/1.c
@@ -0,0 +1,14 @@
+//Example of C function. How to add two number.
+#include<stdio.h>
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    int add = sum(10,30);
+
+    printf("Sum is %d", add);
+    return 0;
+}
