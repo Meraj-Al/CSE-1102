8f9da23069e029becaf5da715cc4a24f4d769c8d
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-01 12:25:37 +0600

    Add files via upload

---
+++ Lecture_10-Functions(M)/3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int a = 30,b = 40;
+    int res = sum(a,b);
+
+    printf("Sum is %d",res);
+    return 0;
+}
