abc566a54bcb18598e392eef03dc78b9f9f4e76a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-01 12:25:18 +0600

    Add files via upload

---
+++ Lecture_10-Functions(M)/2.c
@@ -0,0 +1,12 @@
+//Use of Library Function in C
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    double Number;
+    Number = 49;
+    double squareRoot = sqrt(Number);
+    printf("The Square root of %.2f = %.2f", Number, squareRoot);
+    return 0;
+}
