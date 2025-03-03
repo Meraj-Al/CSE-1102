f1519afdc8bc2e274e12fb1122190806427de7fc
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-03 13:27:12 +0600

    Add files via upload

---
+++ Lecture_10-Functions(M)/21.c
@@ -0,0 +1,24 @@
+// C Program to find the Fibonacci Number using Tree Recursion
+// C Program to find the fibonacci number using tree
+// recursion
+#include <stdio.h>
+int fibonacci(int n)
+{
+// Base case
+// Fibonacci of 0 and 1 is the number itself
+if (n <= 1) {
+return n;
+}
+else {
+// Tree recursive calls
+return fibonacci(n - 1) + fibonacci(n - 2);
+}
+}
+int main()
+{
+// function call
+int n = fibonacci(3);
+// print 5th fibonacci number
+printf("%d", n);
+return 0;
+}
