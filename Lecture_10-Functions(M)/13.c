5216444bda1bdf186e21b9e9cd5bdca911306c97
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-04 07:04:22 +0600

    Update 13.c

--- Lecture_10-Functions(M)/13.c
+++ Lecture_10-Functions(M)/13.c
@@ -20,3 +20,24 @@ int main() {
     printf("GCD of %d and %d is %d\n", x, y, z);
     return 0;
 }
+Input:
+yaml
+Copy
+Edit
+Enter two numbers: 56 98
+Process (Using Euclidean Algorithm):
+gcd(56, 98)
+98 % 56 = 42
+B = 56, A = 42
+gcd(42, 56)
+56 % 42 = 14
+B = 42, A = 14
+gcd(14, 42)
+42 % 14 = 0
+B = 14, A = 0
+A is now 0, so B = 14 is returned as the GCD.
+Output:
+csharp
+Copy
+Edit
+GCD of 56 and 98 is 14
