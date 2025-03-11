369a7bf4462793db0c1ead0859343f8441e95923
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-11 09:27:19 +0600

    Update 10.c

--- Lecture_05-Program Control/Practice Problems/10.c
+++ Lecture_05-Program Control/Practice Problems/10.c
@@ -8,3 +8,9 @@ int main()
     return 0;
 }
 //Output is: Val = 200
+
+/*
+The ternary operator is a shorthand for an if-else statement.
+If b is true (nonzero), the expression before the colon (50) is assigned to a.
+If b is false (0), the expression after the colon (200) is assigned to a.
+*/
