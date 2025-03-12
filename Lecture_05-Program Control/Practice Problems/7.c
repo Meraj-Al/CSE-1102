33d7e97f44eb6e63c5557253ad0b89aa09862449
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-12 09:51:25 +0600

    Update 7.c

--- Lecture_05-Program Control/Practice Problems/7.c
+++ Lecture_05-Program Control/Practice Problems/7.c
@@ -7,23 +7,22 @@ int main()
         printf("Yes ");
     else
         printf("No ");
-   printf("%.9f\f", a-3.1);
+   printf("%.9f\d", a-3.1);
     return 0;
 }
 /*
+
+Output:No -0.000000119
+
+float a = 3.1; actually stores 3.0999999 or a similar approximation.
+So, a == 3.1 may evaluate to false, leading to the else block executing.
+
 Here, 3.1 in the source code is treated as a double (double has higher precision than float).
 Since a (which is float) is slightly different from 3.1 (double), the condition fails.
 The result is "No" instead of "Yes".
 
 The second printf statement:
 printf("%.9f\f", a-3.1);
-
-There are two issues here:
-
-Incorrect Escape Sequence \f
-
-\f is a form feed character (used in old printers).
-It should be \n (newline) instead.
 Checking Precision Difference
 
 a - 3.1 should be close to 0 but not exactly 0.
