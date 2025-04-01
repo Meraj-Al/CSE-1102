d76e2bb8c107ebc8dfa1a2955f0bdb4a01713d23
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-01 18:11:54 +0600

    Add files via upload

---
+++ Lecture_09-Characters and Strings/Home Works/5.c
@@ -0,0 +1,4 @@
+//What is the difference between "a" and 'a'?
+Answer: The expression "a" is a string literal, which is stored in the memory as an
+array of two characters, the 'a' and '\0'. On the other hand, the expression 'a' is
+just the single 'a' character and it is represented by its ASCII code.
