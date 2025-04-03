23571634a1b72ef8be98551506bcfc5192e517d2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-03 20:17:04 +0600

    Update 3.c

--- Lecture_08-Pointers/Home Works/3.c
+++ Lecture_08-Pointers/Home Works/3.c
@@ -8,7 +8,7 @@ int *ptr1, *ptr2, i, j, tmp;
 ptr1 = &i;
 ptr2 = &j;
 printf("Enter numbers: ");
-scanf("%d%d", ptr1, ptr2);
+scanf("%d%d", ptr1, ptr2); //uses two pointers to read two integers(Remember not to use & before ptr in scanf when readng a value)
 tmp = *ptr2;
 *ptr2 = *ptr1;
 *ptr1 = tmp;
