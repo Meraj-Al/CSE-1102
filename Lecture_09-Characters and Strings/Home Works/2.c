e34307a1d595f1b8c3f12c69bca45e6e6977d90f
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-07 22:09:22 +0600

    Update 2.c

--- Lecture_09-Characters and Strings/Home Works/2.c
+++ Lecture_09-Characters and Strings/Home Works/2.c
@@ -1,12 +1,20 @@
 //Write a program that displays the upper- and lowercase letters and their ASCII values.
 #include <stdio.h>
-int main()
-{
-int i;
-for(i = 0; i < 26; i++)
-{
-printf("%c (%d)\n", 'a'+i, 'a'+i);
-printf("%c (%d)\n", 'A'+i, 'A'+i);
+
+int main() {
+    int i;
+    for(i = 0; i < 26; i++) {
+        printf("Lowercase: %c (%d)   Uppercase: %c (%d)\n", 
+               'a' + i, 'a' + i, 'A' + i, 'A' + i);
+    }
+    return 0;
 }
-return 0;
-}
+/*
+✅ Sample Output:
+yaml
+Copy code
+Lowercase: a (97)   Uppercase: A (65)
+Lowercase: b (98)   Uppercase: B (66)
+...
+Lowercase: z (122)  Uppercase: Z (90)
+  */
