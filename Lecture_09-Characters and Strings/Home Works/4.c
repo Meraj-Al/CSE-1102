e0b9b616c6fb9350c1d99e6f00e67597c7242289
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-07 22:17:13 +0600

    Update 4.c

--- Lecture_09-Characters and Strings/Home Works/4.c
+++ Lecture_09-Characters and Strings/Home Works/4.c
@@ -1,20 +1,22 @@
 //Write a program that uses getchar() to read characters. The program should display and count the input characters.
 #include <stdio.h>
-int main()
-{
-int ch, sum;
-printf("Enter characters: ");
-sum = 0;
-ch = getchar();
-while(ch != '\n')
-{
-sum++;
-printf("%c", ch);
-ch = getchar();
+
+int main() {
+    int ch, sum = 0;
+
+    printf("Enter characters: ");
+    ch = getchar();//This initializes the ch variable so that the while loop has something to check.
+
+    while(ch != '\n') {
+        sum++;
+        printf("%c", ch);
+        ch = getchar();//This reads the next character after printing the current one, so the loop can continue with fresh input each time.
+    }
+
+    printf("\nTotal number is = %d\n", sum);
+    return 0;
 }
-printf("\nTotal number is = %d\n", sum);
-return 0;
-}
+
 /*
  getchar() returns the input characters one by one until the '\n' char acter is met. Notice that we could combine the two calls to getchar() into one, like this:
 sum = 0;
