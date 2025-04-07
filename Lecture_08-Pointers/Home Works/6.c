e1243667342c9c7b74b1aecfa1ac47bd4e0b31b2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-07 22:02:55 +0600

    Update 6.c

--- Lecture_08-Pointers/Home Works/6.c
+++ Lecture_08-Pointers/Home Works/6.c
@@ -1,16 +1,23 @@
 //What is the output of the following program?
 #include <stdio.h>
+
 int main()
 {
-int *ptr1, **ptr, i = 10, j = 20;
-ptr1 = &i;
-ptr = &ptr1;
-**ptr += 100;
-ptr1 = &j;
-**ptr += 100;
-printf("Val = %d\n", i+j);
-return 0;
+    int *ptr1, **ptr, i = 10, j = 20;
+    
+    ptr1 = &i;     // ptr1 points to i
+    ptr = &ptr1;   // ptr is a pointer to ptr1
+
+    **ptr += 100;  // *ptr = ptr1, **ptr = *ptr1 = i => i = i + 100 = 110
+
+    ptr1 = &j;     // ptr1 now points to j, but ptr still points to ptr1
+    **ptr += 100;  // **ptr = *ptr1 = j => j = j + 100 = 120
+
+    printf("Val = %d\n", i + j);  // i = 110, j = 120 => i + j = 230
+
+    return 0;
 }
+
 /*ptr = ptr1 = &i and **ptr is
 equal to i. Therefore, the statement **ptr += 100; is equivalent to i = i+100 = 10+100 = 110
 With the statement ptr1 = &j; ptr1 points to the address of j, so **ptr is
