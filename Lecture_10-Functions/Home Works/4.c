13cb681ff35bf97819e87dfdec2ab213c7538af6
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-08 08:15:10 +0600

    Update 4.c

--- Lecture_10-Functions/Home Works/4.c
+++ Lecture_10-Functions/Home Works/4.c
@@ -5,25 +5,40 @@ where F(0) = 0 and F(1) = 1. Write a program that reads an integer N between 2
 and 40 and uses the function to display the Nth term.
 */
 #include <stdio.h>
-unsigned int fib(int num);
-int main()
-{
-    int num;
-    do
-    {
-        printf("Enter a number between 2 and 40: ");
-        scanf("%d", &num);
-    }
-    while(num < 2 || num > 40);
-    printf("F(%d) = %d\n", num, fib(num));
-    return 0;
-}
-unsigned int fib(int num)
-{
-    if(num == 0)
+
+// Recursive function to find Nth Fibonacci term
+int fibonacci(int n) {
+    if (n == 0)
         return 0;
-    else if(num == 1)
+    else if (n == 1)
         return 1;
     else
-        return fib(num−1) + fib(num−2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+int main() {
+    int n;
+    
+    printf("Enter an integer (between 2 and 40): ");
+    scanf("%d", &n);
+    
+    if (n >= 2 && n <= 40) {
+        int result = fibonacci(n);
+        printf("The %d-th Fibonacci term is: %d\n", n, result);
+    } else {
+        printf("Invalid input! Please enter a number between 2 and 40.\n");
+    }
+    
+    return 0;
+}
+/*
+Example:
+fibonacci(5)
+= fibonacci(4) + fibonacci(3)
+= (fibonacci(3) + fibonacci(2)) + (fibonacci(2) + fibonacci(1))
+= ((fibonacci(2) + fibonacci(1)) + (fibonacci(1) + fibonacci(0))) + ((fibonacci(1) + fibonacci(0)) + 1)
+= (((1 + 0) + (1)) + ((1) + (0)) + 1)
+= (1 + 1) + (1 + 0) + 1
+= 2 + 1 + 1
+= 5
+*/
