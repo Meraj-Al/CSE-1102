c39ff6b18978ceee1fd796aa293ba010261da518
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-08 06:59:28 +0600

    Update 3.c

--- Lecture_10-Functions/Practice Problems/3.c
+++ Lecture_10-Functions/Practice Problems/3.c
@@ -12,3 +12,13 @@ int f(int a)
 {
 return a+1;
 }
+/*
+🔍 What happens here?
+i = 10
+
+The innermost f(i) becomes f(10) → returns 11
+
+Next: f(f(i)) becomes f(11) → returns 12
+
+Finally: f(f(f(i))) becomes f(12) → returns 13
+*/
