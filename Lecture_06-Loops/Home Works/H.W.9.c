358f7394d2e43390ed7c04601fe15614b9c9eeb9
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-09 14:30:13 +0600

    Update H.W.9.c

--- Lecture_06-Loops/Home Works/H.W.9.c
+++ Lecture_06-Loops/Home Works/H.W.9.c
@@ -2,28 +2,27 @@
 #include <stdio.h>
 
 int main() {
-    int num, i,Prime = 1;
+    int num, i;
 
-    printf("Enter an integer: ");
+    printf("Enter a number: ");
     scanf("%d", &num);
 
     if (num <= 1) {
-        Prime = 0;
-    } else {
+        printf("Not a prime number.\n");
+        return 0;
+    }
 
-        for (i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                Prime = 0;
-                break;
-            }
+    for (i = 2; i < num; i++) {
+        if (num % i == 0) {
+            printf("Not a prime number.\n");
+            return 0;
         }
     }
 
-    if (Prime)
-        printf("%d is a prime number.\n", num);
-    else
-        printf("%d is not a prime number.\n", num);
-
+    printf("Prime number.\n");
     return 0;
 }
+
+
+
 
