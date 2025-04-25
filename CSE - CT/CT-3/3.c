e2855646d667542ed58f55a7fc005cb80eeafebd
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-25 10:49:08 +0600

    Update 3.c

--- CSE - CT/CT-3/3.c
+++ CSE - CT/CT-3/3.c
@@ -2,12 +2,12 @@ Q3: Describe pointer arithmetic in C
 Answer:
 Pointer arithmetic refers to operations that can be performed on pointers, particularly useful when dealing with arrays or dynamic memory. Here's how it works:
 
-Operation	Description
-ptr + i	Moves pointer i elements forward.
-ptr - i	Moves pointer i elements backward.
-ptr1 - ptr2	Gives number of elements between two pointers.
-++ptr	Moves to the next element.
---ptr	Moves to the previous element.
+Operation	    Description
+ptr + i	      Moves pointer i elements forward.
+ptr - i	      Moves pointer i elements backward.
+ptr1 - ptr2	  Gives number of elements between two pointers.
+++ptr	        Moves to the next element.
+--ptr	        Moves to the previous element.
 Example:
 
 int arr[5] = {10, 20, 30, 40, 50};
