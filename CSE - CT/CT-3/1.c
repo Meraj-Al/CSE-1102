e5c3f8b23324c43bdc6f2d08700220d61fdb13f4
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-25 10:24:30 +0600

    Update 1.c

--- CSE - CT/CT-3/1.c
+++ CSE - CT/CT-3/1.c
@@ -2,7 +2,7 @@ Q1: Differentiate while() loop from do...while() loop.
 Answer:
 
 Feature	    while() Loop	                                do...while() Loop
-Condition   check	At the beginning	                    At the end
+Condition   check	At the beginning	                      At the end
 Execution   guarantee	                                    May not execute even once if condition is false	Executes at least once
 Syntax	    while(condition) { code }	                    do { //code } while(condition);
-Use Case	When pre-check is needed before execution	    When at least one execution is needed
+Use Case	When pre-check is needed before execution	      When at least one execution is needed
