d6f337594397de5af7a6f4ee8edf7147f6a1a36e
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-26 08:52:51 +0600

    Update 2.c

--- Lecture_05-Program Control/Practice Problems/2.c
+++ Lecture_05-Program Control/Practice Problems/2.c
@@ -11,3 +11,34 @@ int main()
         printf("%d %d\n", j, i);
     return 0;
 }
+/*
+Write a program that reads three integers and displays them in ascending order.
+#include <stdio.h>
+
+int main() {
+    int a, b, c, temp;
+
+    // Input
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Sorting using simple comparisons
+    if (a > b) {
+        temp = a; a = b; b = temp;
+    }
+
+    if (a > c) {
+        temp = a; a = c; c = temp;
+    }
+
+    if (b > c) {
+        temp = b; b = c; c = temp;
+    }
+
+    // Output in ascending order
+    printf("Numbers in ascending order: %d %d %d\n", a, b, c);
+
+    return 0;
+}
+
+*/
